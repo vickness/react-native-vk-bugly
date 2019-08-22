fc4154d7f73c59033f317d23ffbaae95bafda174
weike <weike886@gmail.com>
2019-08-22 15:04:34 +0800

    add android files

--- lib/android/src/main/java/com/reactlibrary/RNBuglyModule.java
+++ lib/android/src/main/java/com/reactlibrary/RNBuglyModule.java
@@ -4,7 +4,8 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.tencent.bugly.crashreport.CrashReport;
 
 public class RNBuglyModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +19,39 @@ public class RNBuglyModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNBugly";
+  }
+
+  @ReactMethod
+  public void startWithAppId(final String appId, final ReadableMap bugConfig) {
+    if (bugConfig != null) {
+      CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getReactApplicationContext());
+      if (bugConfig.hasKey("channel")) strategy.setAppChannel(bugConfig.getString("channel"));
+      if (bugConfig.hasKey("version")) strategy.setAppVersion(bugConfig.getString("version"));
+      if (bugConfig.hasKey("deviceIdentifier")) strategy.setDeviceID(bugConfig.getString("deviceIdentifier"));
+      if (bugConfig.hasKey("consolelogEnable")) strategy.setBuglyLogUpload(bugConfig.getBoolean("consolelogEnable"));
+      CrashReport.initCrashReport(getReactApplicationContext(), appId, false, strategy);
+    } else {
+      CrashReport.initCrashReport(getReactApplicationContext(), appId, false);
+    }
+  }
+
+  @ReactMethod
+  public void setUserIdentifier(final String userId) {
+    CrashReport.setUserId(userId);
+  }
+
+  @ReactMethod
+  public void updateAppVersion(final String version) {
+    CrashReport.setAppVersion(getReactApplicationContext(), version);
+  }
+
+  @ReactMethod
+  public void setUserValue(final String value, final String key) {
+    CrashReport.putUserData(getReactApplicationContext(), key, value);
+  }
+
+  @ReactMethod
+  public void setTag(final Integer tag) {
+    CrashReport.setUserSceneTag(getReactApplicationContext(), tag);
   }
 }

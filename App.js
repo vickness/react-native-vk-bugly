c660a3d14bd1df71f6c8227416ef1b3d94c29b9d
weike <weike886@gmail.com>
2019-08-22 13:58:51 +0800

    add ios files

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Bugly from './lib/index';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,8 +18,23 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const BuglyAppId = Platform.select({
+  ios: "745756234534241",
+  android: "34861783241672534"
+});
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentDidMount() {
+
+    const config = {
+      debugMode: true
+    };
+
+    Bugly.startWithAppId(BuglyAppId, config)
+  }
+
   render() {
     return (
       <View style={styles.container}>
--- lib/ios/RNBugly.m
+++ lib/ios/RNBugly.m
@@ -1,13 +1,48 @@
 
 #import "RNBugly.h"
+#import "Bugly.h"
+#import "BuglyConfig.h"
 
 @implementation RNBugly
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 RCT_EXPORT_MODULE()
+
+/** 初始化*/
+RCT_EXPORT_METHOD(startWithAppId:(NSString *)appId buglyConfig:(NSDictionary *)buglyConfig) {
+    if (buglyConfig) {
+        BuglyConfig *config = [BuglyConfig new];
+        config.debugMode = buglyConfig[@"debugMode"];
+        config.channel = buglyConfig[@"channel"];
+        config.version = buglyConfig[@"version"];
+        config.deviceIdentifier = buglyConfig[@"deviceIdentifier"];
+        config.blockMonitorEnable = buglyConfig[@"blockMonitorEnable"];
+        config.blockMonitorTimeout = [buglyConfig[@"blockMonitorTimeout"] doubleValue];
+        config.consolelogEnable = buglyConfig[@"consolelogEnable"];
+        [Bugly startWithAppId:appId config:config];
+    } else {
+        [Bugly startWithAppId:appId];
+    }
+}
+
+/** 设置用户标识*/
+RCT_EXPORT_METHOD(setUserIdentifier:(NSString *)userId) {
+    [Bugly setUserIdentifier:userId];
+}
+
+/** 更新版本信息*/
+RCT_EXPORT_METHOD(updateAppVersion:(NSString *)version) {
+    [Bugly updateAppVersion:version];
+}
+
+/** 设置关键数据，随崩溃信息上报*/
+RCT_EXPORT_METHOD(setUserValue:(NSString *)value forKey:(NSString *)key) {
+    [Bugly setUserValue:value forKey:key];
+}
+
+/** 设置标签*/
+RCT_EXPORT_METHOD(setTag:(NSUInteger)tag) {
+    [Bugly setTag:tag];
+}
 
 @end
   

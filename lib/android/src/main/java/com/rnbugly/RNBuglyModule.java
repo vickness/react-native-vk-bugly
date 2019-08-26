84985d8af6435854a955a7a116bf94b822c3760d
weike <weike886@gmail.com>
2019-08-26 16:06:38 +0800

    edit android package name

--- lib/android/src/main/java/com/rnbugly/RNBuglyModule.java
+++ lib/android/src/main/java/com/rnbugly/RNBuglyModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.rnbugly;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- lib/android/src/main/java/com/rnbugly/RNBuglyPackage.java
+++ lib/android/src/main/java/com/rnbugly/RNBuglyPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.rnbugly;
 
 import java.util.Arrays;
 import java.util.Collections;
